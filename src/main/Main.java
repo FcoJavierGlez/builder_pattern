6a1c4a7c346016b0852f1f5df20cb79dba59f6d9
FcoJavierGlez <FJGS_86@hotmail.com>
2021-10-18 09:24:02 +0200

    Singleton pattern

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,13 +2,24 @@ package main;
 
 import builder_pattern.Usuario;
 import builder_pattern.UsuarioBuilder;
+import singleton_pattern.PersonaSingleton;
 
 public class Main {
 
 	public static void main(String[] args) {
+		//builder pattern
 		Usuario user1 = new UsuarioBuilder().build("inventado@hotmail.com", "Nombre1", "666498209", "Calle inventada nº4");
 		
+		//singleton pattern
+		PersonaSingleton persona = PersonaSingleton.instancePersonaSingleton("personaSingleton", "12345678Z", 20); 
+		
+		
+		
+		//builder
 		System.out.println( user1.toString() );
+		
+		//singleton
+		System.out.println( persona.toString() );
 
 	}
 
---
+++ src/singleton_pattern/PersonaSingleton.java
@@ -0,0 +1,60 @@
+package singleton_pattern;
+
+public class PersonaSingleton {
+	
+	private String nombre;
+	private String dni;
+	private int edad;
+	private static PersonaSingleton instanciaPersona;
+
+	private PersonaSingleton(String nombre, String dni, int edad) {
+		this.nombre = nombre;
+		this.dni = dni;
+		this.edad = edad;
+	}
+	
+	public static PersonaSingleton instancePersonaSingleton(String nombre, String dni, int edad) {
+		if (instanciaPersona == null) 
+			instanciaPersona = new PersonaSingleton(nombre, dni, edad);
+
+		return instanciaPersona;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public static PersonaSingleton getInstanciaPersona() {
+		return instanciaPersona;
+	}
+
+	public static void setInstanciaPersona(PersonaSingleton instanciaPersona) {
+		PersonaSingleton.instanciaPersona = instanciaPersona;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonaSingleton [nombre=" + nombre + ", dni=" + dni + ", edad=" + edad + "]";
+	}
+	
+}
